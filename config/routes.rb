6897f5c1dd7c3a810a7e55fb38966eb258a8bd39
JM1234 <jvestopa@up.edu.ph>
2016-07-27 08:03:01 -0300

    Added link to user's profile in rental show.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 	root 'rentals#index'
 
 	post 'rentals/search' => 'rentals#search', as: 'search_rentals'
+	get '/:id', to: 'users#show', as: :profile
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
