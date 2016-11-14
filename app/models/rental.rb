2a8a8deb8f5d8c5f9e03fd86be90ccb06f9a951f
JM1234 <jvestopa@up.edu.ph>
2016-11-14 02:16:29 -0400

    completed functionalities for google map

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,11 +1,7 @@
 class Rental < ActiveRecord::Base
 	belongs_to :user
 
-	geocoded_by :address
-	after_validation :geocode
-
   has_attached_file :image, styles: { medium: "600x450#"}, default_url: "http://www.clker.com/cliparts/4/8/3/5/1384392924101179310Bedroom.svg.med.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	
-
 end
