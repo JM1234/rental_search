221b0dba8473939af95348135ce73bd71d6a2e35
JM1234 <jvestopa@up.edu.ph>
2016-12-11 23:41:06 -0400

    Review scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :rentals, dependent: :destroy
+	has_many :reviews, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110085332) do
+ActiveRecord::Schema.define(version: 20161212032542) do
 
   create_table "rentals", force: :cascade do |t|
     t.string   "price"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20161110085332) do
     t.string   "description"
     t.float    "latitude"
     t.float    "longitude"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
